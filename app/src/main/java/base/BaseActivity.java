fdbd531b6ace1647739fa59508ab52fb90185126
Jhon_Yoset_Luna_Quispe <64209190+JhonYoset@users.noreply.github.com>
2020-10-28 23:03:03 -0500

    Initial commit

---
+++ app/src/main/java/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package base;
+
+public class BaseActivity {
+    protected void Created(){
+
+    }
+    protected void Paused(){
+
+    }
+    protected  void Resumed(){
+
+    }
+    protected void Destroy(){
+        
+    }
+}
